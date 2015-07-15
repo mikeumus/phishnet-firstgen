9b57c925e6f07897a95fd0c1d7e24e41a8fc7723
mikeumus <mikeumus@gmail.com>
2015-07-15 17:05:26 -0400

    chromedriver and screenshot timeout.

--- notes.md
+++ notes.md
@@ -10,3 +10,11 @@ Postgres in Cloud 9:
  - `sudo -s` then `sudo -u postgres psql`
  - createuser -d -P -s pixm
  - createdb -O mike phishingdb
+
+Install psycopg2 for postgres
+- http://stackoverflow.com/a/5450183
+- `sudo apt-get install libpq-dev python-dev`
+- `sudo apt-get install psycopg2`
+
+Using chromedriver
+- http://selenium-python.readthedocs.org/en/latest/faq.html#how-to-use-chromedriver
--- scrapy_phishtank/pishing/tasks.py
+++ scrapy_phishtank/pishing/tasks.py
@@ -19,7 +19,6 @@ from .models import FreshPhish
 # Phishtank API key: http://data.phishtank.com/data/{}/online-valid.json.bz2'.format(PHISHTANK_API_KEY)
 url = 'http://data.phishtank.com/data/{}/online-valid.json'.format(PHISHTANK_API_KEY)
 
-
 @shared_task(name="StorePhishURL")
 def StorePhishURL():
     r = requests.get(url)
@@ -34,13 +33,28 @@ def StorePhishURL():
         if created:
             # freshphish.fresh_id = threat['phish_id']
             freshphish.fresh_target = threat['target']
-            
+
             try:
                 # take screenshot if status code is good
                 if r.status_code != requests.codes.ok:
                     return "Error: Unexpected response {}".format(r)
                 # take screenshot of fresh_url
-                driver = webdriver.Firefox()
+                #driver = webdriver.Firefox()
+                #executable_path="/usr/bin/chromedriver"
+                driver = webdriver.Chrome()
+                # we dont have driver_arguments varialble!!!
+                # selenium just uses the webdriver APIs so why could we use driver_arguments ?
+                # python will raise an error asking where did you come with that varialble XD
+                # I see. but.. I bet it's posible somehow lol
+                # lol coding is so logical not feeling XD
+                # :p
+                # lets try ipython and we call selenium inside of it and th chromderiver too
+                # good idea, why isn't ipython my default when I do `./manage.py shell` on this tower?
+                # becasue you dont have ipython and it runs python interprester :D
+                # :p
+
+                #driver_arguments['chrome_options'].add_argument('--no-sandbox')
+                deriver.set_page_load_timeout(30)
                 driver.get(threat['url'])
                 # save "phishshots" (screenshots) in folder http://stackoverflow.com/questions/27874002/python-selenium-save-screenshot-in-newly-created-folder
                 driver.save_screenshot('phishots/{0}_{1}_screenshot.png'.format(threat['target'], threat['phish_id']))
@@ -48,13 +62,13 @@ def StorePhishURL():
             except requests.exceptions.RequestException as e:
                 # A serious problem happened, like an SSLError or InvalidURL
                 return "Error: {}".format(e)
-                
+
             if threat['online'] == 'yes':
                 freshphish.live_phish = True   # u'online': u'yes'
             # Consider any status other than 2xx an error
             else:
                 freshphish.live_phish = False
-                
+
             freshphish.verification_time = threat['verification_time']
             if threat['details']:
                 freshphish.country = threat['details'][0]['country']
